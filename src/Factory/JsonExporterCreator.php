716dc527c011b2aa7495c16f9840c556e2247ec5
ZeroFaitheCodes <85941986+ZeroFaithe@users.noreply.github.com>
2026-09-08 16:31:55 +0800

    feat: add exporter factory method

---
+++ src/Factory/JsonExporterCreator.php
@@ -0,0 +1,11 @@
+<?php
+    namespace App\Factory;
+    use App\Exporter\JsonFileExporter;
+
+    class JsonExporterCreator extends ExporterCreator
+    {
+        public function createExporter()
+        {
+            return new JsonFileExporter();
+        }
+    }
