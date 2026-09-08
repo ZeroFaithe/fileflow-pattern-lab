1d19be4e5047397d841f019ebf1281455452a037
ZeroFaitheCodes <85941986+ZeroFaithe@users.noreply.github.com>
2026-09-08 16:45:57 +0800

    feat: add exporter factory method

---
+++ src/Factory/TextExporterCreator.php
@@ -0,0 +1,11 @@
+<?php
+    namespace App\Factory;
+    use App\Exporter\TextFileExporter;
+
+    class TextExporterCreator extends ExporterCreator
+    {
+        public function createExporter()
+        {
+            return new TextFileExporter();
+        }
+    }
